520d37559b63673e9fc3625adfeb4cb013927320
Nicholas Rowan <55177255+nickrow11@users.noreply.github.com>
2019-10-06 15:45:55 -0500

    Add files via upload
    Application Prototype w/ Scaling

---
+++ Application/Application/Application/Application/Application/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Admin on 9/29/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var LightsButton: UIButton!
+    @IBOutlet weak var PowerButton: UIButton!
+    @IBOutlet weak var AudioButton: UIButton!
+    @IBOutlet weak var VibrateButton: UIButton!
+    @IBOutlet weak var TempButton: UIButton!
+    @IBOutlet weak var BatteryButton: UIButton!
+    @IBOutlet weak var TitleBar: UITextView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        PowerButton.layer.cornerRadius = 10
+        LightsButton.layer.cornerRadius = 10
+        AudioButton.layer.cornerRadius = 10
+        VibrateButton.layer.cornerRadius = 10
+        TempButton.layer.cornerRadius = 10
+        BatteryButton.layer.cornerRadius = 10
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
